d69e3f06b220dc3a923b81b796bb5ca40630fd9f
RutwikPatil2024 <rutwikpatil8000@gmail.com>
2024-08-04 20:57:30 +0530

    Data Values

---
+++ valuetype.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8;
+
+contract valuetypes{
+    bool public boolvalue=true;
+    
+    uint public temp=243;
+    
+    int public temp2=1000;
+
+    address public addr=0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+    bytes32 public b32;
+    
+}
