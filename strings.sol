ad0554014ebf079e42b4014658470b422f9ff697
RutwikPatil2024 <rutwikpatil8000@gmail.com>
2024-08-10 12:34:59 +0530

    Strings in solidity

---
+++ strings.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract Strings{
+    string public name="Rutwik Patil";
+
+    function CheckString() public pure returns(string memory){
+        string memory names="aman";
+        return names;
+    }
+}
