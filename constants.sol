eca0de9da24917c94346fbceb5dc7fbb0dd94ab4
RutwikPatil2024 <rutwikpatil8000@gmail.com>
2024-08-10 12:49:58 +0530

    Constants

---
+++ constants.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract Constants{
+    address public constant ownerid=0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    address public constant ownerid2=0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+}
