8fe5942a489b68aa6a4c4a75f68be173eae1e85f
RutwikPatil2024 <rutwikpatil8000@gmail.com>
2024-08-10 12:28:33 +0530

    Default Values in Variable

---
+++ DeafultVal.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract defaultValue{
+    uint public x;
+    bool public flag;
+    address public add;
+    bytes32 public byteS;
+    string public str;
+}
