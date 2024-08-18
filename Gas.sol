d2565e08603dbb93da6dde64ba47eaf8c4ab1302
RutwikPatil2024 <rutwikpatil8000@gmail.com>
2024-08-18 21:38:44 +0530

    Gas

---
+++ Gas.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract Gas{
+    uint public xyzyzgb=0;
+    function InfiniteLoop()public {
+        while(xyzyzgb<=454){
+            xyzyzgb+=2;
+        }
+    }
+}
