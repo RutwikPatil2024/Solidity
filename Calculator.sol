f3a479ad6124f686b3efe246c027bfcce70527ef
RutwikPatil2024 <rutwikpatil8000@gmail.com>
2024-08-18 22:02:42 +0530

    Calculator

---
+++ Calculator.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract Calci{
+    function Addition(uint number1,uint number2)public pure returns(uint){
+        return number1+number2;
+    }
+
+    function Subtraction(uint number1,uint number2)public pure returns(uint){
+        return number1+number2;
+    }
+
+    function Multiplication(uint number1,uint number2)public pure returns(uint){
+        return number1+number2;
+    }
+
+    function Division(uint number1,uint number2)public pure returns(uint){
+        return number1+number2;
+    }
+}
